48374e09dd6da2a4d68673de680f9b2aa75a49ce
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-01-28 11:07:21 -0600

    instaling the dotenv

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 // Load Express 
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
-let PORT = 3000;
+let PORT = process.env.PORT || 3000;
 
 app.listen(PORT, (req, res) => {
     console.log (`Server Start Listening to the Port No. : ${PORT}`);
